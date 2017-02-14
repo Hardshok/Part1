e3904c73ddb9b5ad8e006fa2d6ea849fb5ca6458
Sasha Shchepinskyi <hardshoks@gmail.com>
2017-02-15 00:55:08 +0200

    Lesson4 - Sort Array

--- Lesson4/SortArray/SortArray/Program.cs
+++ Lesson4/SortArray/SortArray/Program.cs
@@ -10,19 +10,73 @@ namespace SortArray
     {
         static void Main()
         {
-            Console.WriteLine("Enter the size of array:");
-            int[] userArray = new int[ReadAndValidate(false)]; // 'false' arg. means Zero is not allowed as the size of the array
+            int[] userArray = InitArray();  
+            bool bRepeat=false;
+            do {
+                Console.WriteLine("Sort method:\n 1=Bubble sorting by Ascending\n 2=Bubble sorting by Descending");
+                Console.Write("Please enter your selection:");
+                int iVariant = ReadAndValidate(false);
 
-            Console.WriteLine("\nEnter the array's elements (each number to separate by Enter):");
-            for (int iCount = 0; iCount < userArray.Length; iCount++)
-                userArray[iCount] = ReadAndValidate();
-                        
+                switch (iVariant)
+                {
+                    case 1:
+                        Console.WriteLine("Bubble sorting by Ascending");
+                        int[] Array1 = userArray;
+                        BubbleSortAscending(Array1);
+                        PrintArray(Array1);
+                        break;
+                    case 2:
+                        Console.WriteLine("Bubble sorting by Descending");
+                        int[] Array2 = userArray;
+                        BubbleSortDescending(Array2);
+                        PrintArray(Array2);
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Invalid selection. Please select 1, 2, 3, or 4");
+                        break;
+                }
+
+                Console.WriteLine("\nDo you want to sort array again? Y/N");
+                string sRepeat = Console.ReadLine();
+                if (sRepeat == "y" || sRepeat == "Y")
+                    bRepeat = true;
+                else 
+                    bRepeat = false;
+            } while (bRepeat);
+                
+
+                                    
             //BubbleSortAscending(userArray);
-            BubbleSortDescending(userArray);       
+                   
 
             Console.WriteLine("\nPress any key to exit...");
             
             Console.ReadKey();
+        }
+
+        static int[] InitArray() 
+        {
+            Console.WriteLine("Enter the size of array:");
+            int[] userArray = new int[ReadAndValidate(false)]; // 'false' arg. means Zero is not allowed as the size of the array
+            Console.WriteLine("\nEnter the array's elements (each number to separate by Enter):");
+            for (int iCount = 0; iCount < userArray.Length; iCount++)
+                userArray[iCount] = ReadAndValidate();
+            return userArray;
+        }
+
+        static void PrintArray(int[] Array)
+        {
+            Console.WriteLine("Array is:");
+            string Sum="";
+            for (int iCount = 0; iCount < Array.Length; iCount++)
+            {
+                Sum += Array[iCount]+ "; ";
+            }
+            Console.WriteLine(Sum);
         }
 
         //ReadLine() + validate that the entered value is: not a string; not empty, not out of the integer range; not 0 (if isZeroAllowed=True)
