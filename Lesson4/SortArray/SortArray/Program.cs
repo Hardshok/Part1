42c0bee2aa0afe8241dbe9124df9571590ec7104
Oleksandrs <hardshoks@gmail.com>
2017-02-17 13:50:25 +0200

    Lesson 4 - One project: Sort, Stack, Queue

--- Lesson4/SortArray/SortArray/Program.cs
+++ Lesson4/SortArray/SortArray/Program.cs
@@ -8,8 +8,10 @@ namespace SortArray
 {    
     static class Program
     {
-        static int top;        
-
+        /* 'Tail': start place in array to read
+         * 'Head': place to which write new data */
+        static int top, Tail, Head, Count;
+        
         static void Main()
         {            
             Console.Title= "SORT ARRAY METHODS, STACK AMD QUEUE";
@@ -21,15 +23,13 @@ namespace SortArray
                 WriteColorLine(ConsoleColor.Yellow, "Modes:\n 1 = Sort Array: Two Methods\n 2 = Stack API with array\n 3 = Queue API with array");
                 Console.Write("Please enter your selection: ");                
                 int iMode = ReadAndValidate(false);
-                switch (iMode)
-                {
+                switch (iMode) {
                     case 1: //  TWO SORTONG METHODS FOR ARRAY
                         Console.Clear();
                         WriteColorLine(ConsoleColor.Blue, "*** Sort Array: Two Methods***\n ");
                         int[] userArray = InitArray();
                         bool bRepeat = false;
-                        do
-                        {    // user can select sorting method again
+                        do {    // user can select sorting method again
                             WriteColorLine(ConsoleColor.Yellow, "\nSort method:\n 1 = Bubble sorting by Ascending\n 2 = Bubble sorting by Descending");
                             WriteColorLine(ConsoleColor.Yellow, " 3 = Insertion Sort by Ascending\n 4 = Insertion Sort by Descending");
                             Console.Write("Please enter your selection: ");
@@ -72,8 +72,7 @@ namespace SortArray
                         WriteColorLine(ConsoleColor.Blue, "*** Implement Stack ***\n");
                         string[] userStack = InitStack();
                         bool bAction = false;
-                        do
-                        { //user can repeat operations with Stack
+                        do { //user can repeat operations with Stack
                             WriteColorLine(ConsoleColor.Yellow, "\n 1 = Push Stack\n 2 = Pop Stack\n 3 = Pick\n 4 = Stack State (IsEmpty, IsFull)");
                             Console.Write("Select operation for stack: ");
                             int iOperation = ReadAndValidate(false);
@@ -117,12 +116,10 @@ namespace SortArray
 
                     case 3: //IMPLEMENT CIRCULAR BUFFER QUEUE
                         Console.Clear();
-                        WriteColorLine(ConsoleColor.Blue, "*** Implement Circular Buffer Queue ***\n");
-                        Queue Queue = new Queue();
-                        string[] userQueue = Queue.InitQueue();
+                        WriteColorLine(ConsoleColor.Blue, "*** Implement Circular Buffer Queue ***\n");                                                
+                        string[] userQueue = InitQueue();
                         bool bRepeatQueue = false;
-                        do
-                        {  //user can repeat operations with Queue
+                        do {  //user can repeat operations with Queue
                             WriteColorLine(ConsoleColor.Yellow, "\n 1 = Enqueue\n 2 = Dequeue\n 3 = IsEmpty\n 4 = IsFull");
                             Console.Write("Select operation for stack: ");
                             int iOperation = ReadAndValidate(false);
@@ -152,10 +149,10 @@ namespace SortArray
             } while (bMainMenu);
             WriteColorLine(ConsoleColor.White, "Press any key to exit...");
             Console.ReadKey();
-        } 
+        }
 
-                
-        // ************* Array Sorting ***************
+        #region ****** ARRAY SORTING ******
+
         static void BubbleSortAscending(int[] argArray) 
         {
             bool b;
@@ -225,29 +222,11 @@ namespace SortArray
             int iBuffer = argArray[argIndex1];
             argArray[argIndex1] = argArray[argIndex2];
             argArray[argIndex2] = iBuffer;
-        }
-
-        // ReadAndValidate() method:
-        // Do ReadLine + validate that the entered value is: not a string; not empty, not out of the integer range; not 0 (if isZeroAllowed=True)
-        public static int ReadAndValidate(bool isZeroAllowed = true)
-        {
-            int iLine;
-            if (!isZeroAllowed) //entering '0' is not allowed
-            {
-                while (Int32.TryParse(Console.ReadLine(), out iLine) == false || iLine == 0)
-                    WriteColorLine(ConsoleColor.Red, "The value is equal '0', not a number or out of the range. Enter it again:");
-            }
-            else
-            {
-                while (Int32.TryParse(Console.ReadLine(), out iLine) == false)
-                    WriteColorLine(ConsoleColor.Red, "The value is not a number or out of the range. Try to enter it again:");
-            }
-            return iLine;
-        }
+        }        
 
         static int[] InitArray()
         {
-            Console.Write("Enter the size of array: ");
+            Console.Write("Enter the SIZE of array: ");
             int[] userArray = new int[ReadAndValidate(false)]; // 'false' arg. means Zero is not allowed as the size of the array
             Console.WriteLine("\nEnter the array's elements (each number to separate by Enter):");
             for (int iCount = 0; iCount < userArray.Length; iCount++)
@@ -265,23 +244,9 @@ namespace SortArray
             }
             WriteColorLine(ConsoleColor.Green, Sum);            
         }
+        #endregion
 
-        static void WriteColorLine(ConsoleColor sColor, string sMessage)
-        {
-            Console.ForegroundColor = sColor;
-            Console.WriteLine(sMessage);            
-            Console.ResetColor();
-        }
-
-        static bool IsRepeat (string message) //Service function
-        {            
-            WriteColorLine(ConsoleColor.White, "\n"+message+" Y/N");
-            string sRepeat = Console.ReadLine();
-            bool bRepeat = (sRepeat == "y" || sRepeat == "Y") ? true : false;
-            return bRepeat;
-        }
-
-        //****************** STACK API **************
+        #region ****** STACK API ******
         static void StackPush(string[] Stack, string sMessage)
         {
             if (!StackIsFull(Stack))
@@ -330,7 +295,7 @@ namespace SortArray
 
         static string[] InitStack()
         {
-            Console.Write("Enter the size of Stack: ");
+            Console.Write("Enter the SIZE of Stack: ");
             string[] Stack = new string[ReadAndValidate(false)]; // 'false' arg. means Zero is not allowed as the size of the array            
             top = -1;
             Console.WriteLine(" New Stack is created with size {0}", Stack.Length);
@@ -346,6 +311,84 @@ namespace SortArray
                 Sum += Stack[iCount] + "; ";
             }
             WriteColorLine(ConsoleColor.Green, "[ " + Sum + "]");
-        }   
+        }
+        #endregion
+
+        #region ****** QUEUE API ******
+        static string[] InitQueue()
+        {
+            Console.Write("Enter the SIZE of Queue: ");
+            string[] Queue = new string[Program.ReadAndValidate(false)]; // 'false' arg. means Zero is not allowed as the size of the array                        
+            Head = 0;
+            Tail = 0;
+            Count = 0;
+            Console.WriteLine(" New Queue is created with size {0}", Queue.Length);
+            return Queue;
+        }
+
+        static void Enqueue(string[] Stack, string sMessage)  //Put element in queue
+        {
+
+        }
+
+        static string Dequeue(string[] Stack) //Get element from queue (through out parameter, true if element got from queue, false otherwise)
+        {
+            string sMessage = "";
+
+            return sMessage;
+        }
+
+        static bool IsEmpty() //Return true if buffer is empty
+        {
+            bool IsEmpty;
+            IsEmpty = (Count == 0) ? true : false;
+            return IsEmpty;
+        }
+
+        static bool IsFull(string[] userQueue) //Return true if buffer is full
+        {
+            bool IsFull;
+            IsFull = (Count == userQueue.Length) ? true : false;
+            return IsFull;
+        }
+        #endregion
+
+        #region ****** SERVICE METHODS ******    
+
+        static void WriteColorLine(ConsoleColor sColor, string sMessage)
+        {
+            Console.ForegroundColor = sColor;
+            Console.WriteLine(sMessage);
+            Console.ResetColor();
+        }
+        static bool IsRepeat(string message)
+        {
+            WriteColorLine(ConsoleColor.White, "\n" + message + " Y/N");
+            string sRepeat = Console.ReadLine();
+            bool bRepeat = (sRepeat == "y" || sRepeat == "Y") ? true : false;
+            return bRepeat;
+        }
+        
+        /// <summary>
+        /// Read console line and convert it to Int32. User have to enter while it will be not number. By default '0' is allowed. 
+        /// </summary>
+        /// <param name="isZeroAllowed"></param>
+        /// <returns></returns>
+        public static int ReadAndValidate(bool isZeroAllowed = true)
+        {
+            int iLine;
+            if (!isZeroAllowed) //entering '0' is not allowed
+            {
+                while (Int32.TryParse(Console.ReadLine(), out iLine) == false || iLine == 0)
+                    WriteColorLine(ConsoleColor.Red, "The value is equal '0', not a number or out of the range. Enter it again:");
+            }
+            else
+            {
+                while (Int32.TryParse(Console.ReadLine(), out iLine) == false)
+                    WriteColorLine(ConsoleColor.Red, "The value is not a number or out of the range. Try to enter it again:");
+            }
+            return iLine;
+        }
+        #endregion
     }
 }
